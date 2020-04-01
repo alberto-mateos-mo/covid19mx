2e186e6b4c3f1e664611a6f42126b88dbd6c4acd
David Mateos <cldav.privmath@gmail.com>
2020-03-31 19:41:44 -0600

    script to prepare the data

--- data-raw/covid19mx_data.R
+++ data-raw/covid19mx_data.R
@@ -1,36 +1,8 @@
-## code to prepare `covid19mx_data` dataset goes here
+## code to prepare covid19 MX data
+## The data needs to be updated daily
 
 require(pdftools)
 require(tidyverse)
-require(ggplot2)
-require(unam.theme)
-
-strsplit2 <- function(x,
-                      split,
-                      type = "remove",
-                      perl = FALSE,
-                      ...) {
-  if (type == "remove") {
-    # use base::strsplit
-    out <- base::strsplit(x = x, split = split, perl = perl, ...)
-  } else if (type == "before") {
-    # split before the delimiter and keep it
-    out <- base::strsplit(x = x,
-                          split = paste0("(?<=.)(?=", split, ")"),
-                          perl = TRUE,
-                          ...)
-  } else if (type == "after") {
-    # split after the delimiter and keep it
-    out <- base::strsplit(x = x,
-                          split = paste0("(?<=", split, ")"),
-                          perl = TRUE,
-                          ...)
-  } else {
-    # wrong type input
-    stop("type must be remove, after or before!")
-  }
-  return(out)
-}
 
 url_sospechosos <- "https://www.gob.mx/cms/uploads/attachment/file/544539/Tabla_casos_sospechosos_COVID-19_2020.03.31.pdf"
 
@@ -78,4 +50,6 @@ fechas <- str_extract(doc, pattern = "\\d{1,2}\\/\\d{1,2}\\/\\d{4}")
 
 positivos_df <- data.frame(caso = 1:length(doc), estado = estados, genero = generos, edad = edades, fecha = fechas)
 
-usethis::use_data("covid19mx_data")
+casos_positivos <- positivos_df
+
+usethis::use_data(casos_positivos)
