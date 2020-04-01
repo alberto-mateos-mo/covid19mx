1bcbc777b3c90e93834a076cfabd1852fa4435c4
David Mateos <cldav.privmath@gmail.com>
2020-04-01 00:34:49 -0600

    Data download module

---
+++ R/mod_data_download.R
@@ -0,0 +1,49 @@
+# Module UI
+  
+#' @title   mod_data_download_ui and mod_data_download_server
+#' @description  A shiny Module.
+#'
+#' @param id shiny id
+#' @param input internal
+#' @param output internal
+#' @param session internal
+#'
+#' @rdname mod_data_download
+#'
+#' @keywords internal
+#' @export 
+#' @importFrom shiny NS tagList 
+mod_data_download_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    mainPanel(
+      DT::DTOutput(ns("datos"))
+    )
+  )
+}
+    
+# Module Server
+    
+#' @rdname mod_data_download
+#' @export
+#' @keywords internal
+    
+mod_data_download_server <- function(input, output, session){
+  ns <- session$ns
+  
+  output$datos <- DT::renderDT({
+    DT::datatable(casos_positivos,
+                  extensions = 'Buttons',
+                  options = list(
+                    dom = 'Bfrtip',
+                    buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
+                  ))
+  })
+}
+    
+## To be copied in the UI
+# mod_data_download_ui("data_download_ui_1")
+    
+## To be copied in the server
+# callModule(mod_data_download_server, "data_download_ui_1")
+ 
