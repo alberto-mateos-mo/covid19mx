95bf79ac6f9f898bd66241218fd3d6edf642c961
David Mateos <cldav.privmath@gmail.com>
2020-03-31 20:09:55 -0600

    new ui

--- R/app_ui.R
+++ R/app_ui.R
@@ -4,8 +4,11 @@ app_ui <- function() {
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # List the first level UI elements here 
-    fluidPage(
-      h1("covid19mx")
+    navbarPage(
+      title = ("covid19 MX"),
+      tabPanel("Data Explorer",
+               mod_data_vis_ui("data_vis_ui_1")
+               )
     )
   )
 }
@@ -17,9 +20,15 @@ golem_add_external_resources <- function(){
     'www', system.file('app/www', package = 'covid19mx')
   )
  
+  bootstraplib::bs_theme_new(version = "4+3", bootswatch = "lux")
+  bootstraplib::bs_theme_add_variables(`font-size-base` = "1rem")
+  bootstraplib::bs_theme_add_variables(`body-color` = "#002B7A", 
+                                       `input-border-color` = "#002B7A", primary = "#D59F0F", 
+                                       default = "#D59F0F", secondary = "#D59F0F", `gray-900` = "#002B7A")
+  
   tags$head(
     golem::activate_js(),
-    golem::favicon()
+    bootstraplib::bootstrap(minified = FALSE)
     # Add here all the external resources
     # If you have a custom.css in the inst/app/www
     # Or for example, you can add shinyalert::useShinyalert() here
