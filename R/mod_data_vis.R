a30f1793c5fc6a25dc24334a377e6154479e70a5
David Mateos <cldav.privmath@gmail.com>
2020-04-01 00:25:24 -0600

    data vis module ready

--- R/mod_data_vis.R
+++ R/mod_data_vis.R
@@ -16,8 +16,19 @@
 mod_data_vis_ui <- function(id){
   ns <- NS(id)
   tagList(
-    mainPanel(
-      plotOutput(ns("casos_acum"))
+    tabsetPanel(
+      tabPanel("Comportamiento",
+               fluidRow(
+                 col_6(plotly::plotlyOutput(ns("casos_acum"))),
+                 col_6(plotly::plotlyOutput(ns("casos_freq")))
+               )
+               ),
+      tabPanel("Demográficos",
+               fluidRow(
+                 col_6(plotly::plotlyOutput(ns("casos_gen"))),
+                 col_6(plotly::plotlyOutput(ns("casos_edad")))
+               )
+               )
     )
   )
 }
@@ -30,6 +41,66 @@ mod_data_vis_ui <- function(id){
     
 mod_data_vis_server <- function(input, output, session){
   ns <- session$ns
+  
+  casos_fecha <- table(casos_positivos$fecha) %>% 
+    as.data.frame()
+  
+  casos_fecha <- janitor::clean_names(casos_fecha)
+  
+  names(casos_fecha) <- c("fecha", "freq")
+  
+  casos_fecha <- casos_fecha %>% 
+    mutate(fecha = lubridate::dmy(fecha))
+  
+  casos_fecha <- arrange(casos_fecha, fecha)
+  
+  casos_fecha <- casos_fecha %>% 
+    mutate(cum_freq = cumsum(freq))
+  
+  output$casos_acum <- plotly::renderPlotly({
+    
+    plotly::ggplotly(ggplot(casos_fecha)+
+                       geom_line(aes(fecha, cum_freq))+
+                       geom_point(aes(fecha, cum_freq))+
+                       scale_x_date(breaks = "week")+
+                       labs(x = "", y = "")+
+                       ggtitle("Casos acumulados por fecha de inicio de síntomas")+
+                       theme_unam()+
+                       theme(axis.text.x = element_text(angle = 90)))
+  })
+  
+  output$casos_freq <- plotly::renderPlotly({
+    plotly::ggplotly(ggplot(casos_fecha)+
+                       geom_line(aes(fecha, freq))+
+                       geom_point(aes(fecha, freq))+
+                       scale_x_date(breaks = "week")+
+                       labs(x = "", y = "")+
+                       ggtitle("Casos por fecha de inicio de síntomas")+
+                       theme_unam()+
+                       theme(axis.text.x = element_text(angle = 90)))
+  })
+  
+  output$casos_gen <- plotly::renderPlotly({
+    plotly::ggplotly(ggplot(casos_positivos)+
+                       geom_bar(aes("genero", fill = genero), position = "fill")+
+                       coord_flip()+
+                       labs(x = "", y = "")+
+                       ggtitle("Casos por género")+
+                       theme_unam()+
+                       scale_fill_discrete("Género")+
+                       theme(axis.text = element_blank(), 
+                             panel.grid = element_blank(), 
+                             axis.ticks = element_blank(), 
+                             axis.line = element_blank()))
+  })
+  
+  output$casos_edad <- plotly::renderPlotly({
+    plotly::ggplotly(ggplot(casos_positivos)+
+                       geom_bar(aes(edad))+
+                       labs(x = "Edad", y = "Casos")+
+                       ggtitle("Casos por edad")+
+                       theme_unam())
+  })
   
 }
     
