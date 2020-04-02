d9ca3ed12e5506477c561ed5faaa0c11f69978e4
David Mateos <cldav.privmath@gmail.com>
2020-04-01 19:04:56 -0600

    data updated 01-04-2020

--- data-raw/covid19mx_data.R
+++ data-raw/covid19mx_data.R
@@ -4,11 +4,11 @@
 require(pdftools)
 require(tidyverse)
 
-url_sospechosos <- "https://www.gob.mx/cms/uploads/attachment/file/544539/Tabla_casos_sospechosos_COVID-19_2020.03.31.pdf"
+#url_sospechosos <- "https://www.gob.mx/cms/uploads/attachment/file/544539/Tabla_casos_sospechosos_COVID-19_2020.03.31.pdf"
 
 download.file(url_sospechosos, 'casos_sospechosos.pdf', mode="wb")
 
-url_positivos <- "https://www.gob.mx/cms/uploads/attachment/file/544538/Tabla_casos_positivos_COVID-19_resultado_InDRE_2020.03.31.pdf"
+url_positivos <- "https://www.gob.mx/cms/uploads/attachment/file/544743/Tabla_casos_positivos_COVID-19_resultado_InDRE_2020.04.01.pdf"
 
 download.file(url_positivos, "casos_positivos.pdf", mode = "wb")
 
