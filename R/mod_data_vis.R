2eac71b49f2996541b1a3366e4f8bfb460f4cbdd
David Mateos <cldav.privmath@gmail.com>
2020-05-08 19:48:48 -0500

    update 08-05-2020

--- R/mod_data_vis.R
+++ R/mod_data_vis.R
@@ -164,7 +164,11 @@ mod_data_vis_server <- function(input, output, session){
   })
   
   output$n_decesos <- renderText({
-    scales::comma(nrow(covid_data[covid_data$resultado == "Positivo SARS-CoV-2"&covid_data$fecha_def != "9999-99-99",]))
+    if(input$estado == "NACIONAL"){
+      scales::comma(nrow(covid_data[covid_data$resultado == "Positivo SARS-CoV-2"&covid_data$fecha_def != "9999-99-99",]))
+    }else{
+      scales::comma(nrow(covid_data[covid_data$resultado == "Positivo SARS-CoV-2"&covid_data$fecha_def != "9999-99-99"&covid_data$entidad_res == input$estado,])) 
+    }
   })
   
 }
