58fcb7c831952fb00224dfa3fb883f7b14698bdc
David Mateos <cldav.privmath@gmail.com>
2020-07-23 20:49:14 -0500

    update 23-07-2020

--- R/mod_data_tables.R
+++ R/mod_data_tables.R
@@ -48,7 +48,7 @@ mod_data_tables_server <- function(input, output, session){
       dplyr::filter(resultado == "Positivo SARS-CoV-2") %>% 
       dplyr::filter(entidad_res == input$estado) %>% 
       dplyr::group_by(municipio_res) %>% 
-      dplyr::summarise(casos = n()) %>% 
+      dplyr::summarise(casos = dplyr::n()) %>% 
       dplyr::mutate(casos_estimados = round(casos*(104562/4524)))
     
     b <- covid_data %>% 
@@ -56,7 +56,7 @@ mod_data_tables_server <- function(input, output, session){
       dplyr::filter(entidad_res == input$estado) %>% 
       dplyr::filter(fecha_def != "9999-99-99") %>% 
       dplyr::group_by(municipio_res) %>% 
-      dplyr::summarise(defunciones = n())
+      dplyr::summarise(defunciones = dplyr::n())
     
     dplyr::full_join(a,b)
     
