57024b4415a002a16cc7adf8e05f9c181799f957
David Mateos <cldav.privmath@gmail.com>
2020-09-11 22:12:28 -0500

    update 11-09-2020

--- R/app_ui.R
+++ R/app_ui.R
@@ -19,7 +19,7 @@ app_ui <- function() {
                # mod_data_download_ui("data_download_ui_1")
                # ),
       inverse = TRUE,
-      footer = "Fecha de actualización: 10/09/2020"
+      footer = "Fecha de actualización: 11/09/2020"
     )
   )
 }
